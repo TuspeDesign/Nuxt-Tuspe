be0c55f78351e41ebbede56a35bf94356a68d8de
timoanttila <14306628+timoanttila@users.noreply.github.com>
2021-12-14 09:30:39 +0200

    Nuxt starter

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<Tutorial />
+</template>
+
+<script lang="ts">
+	import Vue from "vue";
+	import Json from "";
+
+	export default Vue.extend({
+		data() {
+			return {
+				page: null,
+			};
+		},
+		async fetch() {
+			const temp = await fetch(``);
+		},
+	});
+</script>
